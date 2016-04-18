9543d81e405438fbc512d62d1b65414518965a84
Oksana Slusarenro <oksana.slu@gmail.com>
2016-04-18 15:22:28 +0300

    Add grade for one page SCORM.

--- scormxblock/scormxblock.py
+++ scormxblock/scormxblock.py
@@ -8,7 +8,7 @@ from django.conf import settings
 from webob import Response
 
 from xblock.core import XBlock
-from xblock.fields import Scope, Integer, String
+from xblock.fields import Scope, Integer, String, Float
 from xblock.fragment import Fragment
 
 # Make '_' a no-op so we can scrape strings
@@ -16,6 +16,8 @@ _ = lambda text: text
 
 
 class ScormXBlock(XBlock):
+
+    has_score = True
 
     display_name = String(
         display_name=_("Display Name"),
@@ -28,6 +30,20 @@ class ScormXBlock(XBlock):
         scope=Scope.settings,
     )
 
+    lesson_status = String(
+        scope=Scope.user_state,
+        default='not attempted'
+    )
+    lesson_score = Float(
+        scope=Scope.user_state,
+        default=0
+    )
+
+    weight = Float(
+        default=1,
+        scope=Scope.settings
+    )
+
     def resource_string(self, path):
         """Handy helper for getting resources from our kit."""
         data = pkg_resources.resource_string(__name__, path)
@@ -36,7 +52,7 @@ class ScormXBlock(XBlock):
     def student_view(self, context=None):
         scorm_file = 'http://{}{}'.format(settings.ENV_TOKENS.get('LMS_BASE'), self.scorm_file)
         html = self.resource_string("static/html/scormxblock.html")
-        frag = Fragment(html.format(scorm_file=scorm_file))
+        frag = Fragment(html.format(scorm_file=scorm_file, self=self))
         frag.add_css(self.resource_string("static/css/scormxblock.css"))
         frag.add_javascript(self.resource_string("static/js/src/scormxblock.js"))
         frag.initialize_js('ScormXBlock')
@@ -64,6 +80,44 @@ class ScormXBlock(XBlock):
                                            '{}/index.html'.format(self.location.block_id))
 
         return Response(json.dumps({'result': 'success'}), content_type='application/json')
+
+    @XBlock.json_handler
+    def scorm_get_value(self, data, suffix=''):
+        name = data.get('name')
+        if name == 'cmi.core.lesson_status':
+            return {'value': self.lesson_status}
+        return {'value': ''}
+
+    @XBlock.json_handler
+    def scorm_set_value(self, data, suffix=''):
+        context = {'result': 'success'}
+        name = data.get('name')
+        if name == 'cmi.core.lesson_status' and data.get('value') != 'completed':
+            self.lesson_status = data.get('value')
+            self.publish_grade()
+            context.update({"lesson_score": self.lesson_score})
+        if name == 'cmi.core.score.raw':
+            self.lesson_score = int(data.get('value', 0))/100.0
+        return context
+
+    def publish_grade(self):
+        if self.lesson_status == 'passed':
+            self.runtime.publish(
+                self,
+                'grade',
+                {
+                    'value': self.lesson_score,
+                    'max_value': self.weight,
+                })
+        if self.lesson_status == 'failed':
+            self.runtime.publish(
+                self,
+                'grade',
+                {
+                    'value': 0,
+                    'max_value': self.weight,
+                })
+            self.lesson_score = 0
 
     @staticmethod
     def workbench_scenarios():
--- scormxblock/static/js/src/scormxblock.js
+++ scormxblock/static/js/src/scormxblock.js
@@ -1,3 +1,76 @@
 function ScormXBlock(runtime, element) {
 
+  function SCORM_API(){
+
+    this.LMSInitialize = function(){
+      console.log('LMSInitialize');
+      return true;
+    };
+
+    this.LMSFinish = function() {
+      console.log("LMSFinish");
+      return "true";
+    };
+
+    this.LMSGetValue = function(cmi_element) {
+      console.log("Getvalue for " + cmi_element);
+      var handlerUrl = runtime.handlerUrl(element, 'scorm_get_value');
+
+      var response = $.ajax({
+        type: "POST",
+        url: handlerUrl,
+        data: JSON.stringify({'name': cmi_element}),
+        async: false
+      });
+      response = JSON.parse(response.responseText);
+      return response.value
+    };
+
+    this.LMSSetValue = function(cmi_element, value) {
+      console.log("LMSSetValue " + cmi_element + " = " + value);
+      var handlerUrl = runtime.handlerUrl( element, 'scorm_set_value');
+
+      if (cmi_element == 'cmi.core.lesson_status'||cmi_element == 'cmi.core.score.raw'){
+
+        $.ajax({
+          type: "POST",
+          url: handlerUrl,
+          data: JSON.stringify({'name': cmi_element, 'value': value}),
+          async: false,
+          success: function(response){
+            if (typeof response.lesson_score != "undefined"){
+              $(".lesson_score", element).html(response.lesson_score);
+            }
+          }
+        });
+
+      }
+      return true;
+    };
+
+    this.LMSCommit = function() {
+        console.log("LMSCommit");
+        return "true";
+    };
+
+    this.LMSGetLastError = function() {
+      console.log("GetLastError");
+      return "0";
+    };
+
+    this.LMSGetErrorString = function(errorCode) {
+      console.log("LMSGetErrorString");
+      return "Some Error";
+    };
+
+    this.LMSGetDiagnostic = function(errorCode) {
+      console.log("LMSGetDiagnostic");
+      return "Some Diagnostice";
+    }
+  }
+
+  $(function ($) {
+    API = new SCORM_API();
+    console.log("Initial SCORM data...");
+  });
 }
