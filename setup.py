e53438e75aa63d71fce149dbe3e9a3977d589457
Oksana Slusarenko <oksana.slu@gmail.com>
2018-02-12 18:46:27 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def package_data(pkg, roots):
 
 setup(
     name='scormxblock-xblock',
-    version='0.1',
+    version='0.2',
     description='scormxblock XBlock',   # TODO: write a better description.
     packages=[
         'scormxblock',
